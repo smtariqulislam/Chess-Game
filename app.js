7065a7b1fb5f169e565adb09ade2a0452a2b8c32
S M Tariqul Islam <s.m.tariqul.islam.eu181400082@gmail.com>
2023-11-04 17:27:01 +0600

    last update

--- app.js
+++ app.js
@@ -75,27 +75,98 @@ const startPieces = [
   rook,
 ];
 
+// const startPieces = [
+//   "rook",
+//   "knight",
+//   "bishop",
+//   "king",
+//   "queen",
+//   "bishop",
+//   "knight",
+//   "rook",
+//   "pawn",
+//   "pawn",
+//   "pawn",
+//   "pawn",
+//   "pawn",
+//   "pawn",
+//   "pawn",
+//   "pawn",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "",
+//   "pawn",
+//   "pawn",
+//   "pawn",
+//   "pawn",
+//   "pawn",
+//   "pawn",
+//   "pawn",
+//   "pawn",
+//   "rook",
+//   "knight",
+//   "bishop",
+//   "king",
+//   "queen",
+//   "bishop",
+//   "knight",
+//   "rook",
+// ];
+
 function createBoard() {
   startPieces.forEach((startPiece, i) => {
     const square = document.createElement("div");
     square.classList.add("square");
     square.innerHTML = startPiece;
-    square.firstChild?.setAttribute("draggable", true);
     square.setAttribute("square-id", i);
+    square.classList.add("beige");
 
-    const row = Math.floor((63 - i) / 8 + 1);
+    square.firstChild?.setAttribute("draggable", true);
+
+    const row = Math.floor((63 - i) / 8) + 1;
 
     if (row % 2 === 0) {
-      square.classList.add(i % 2 === 0 ? "white" : "black");
+      square.classList.add(i % 2 === 0 ? "beige" : "brown");
     } else {
-      square.classList.add(i % 2 === 0 ? "black" : "white");
+      square.classList.add(i % 2 === 0 ? "brown" : "beige");
     }
 
     if (i <= 15) {
-      square.classList.add("black-deep");
+      square.classList.add("black");
+      // square.firstChild.firstChild.classList.add("black");
     }
     if (i >= 48) {
-      square.classList.add("white-deep");
+      square.classList.add("white");
+      // square.firstChild.firstChild.classList.add("white");
     }
 
     gameBoard.append(square);
@@ -103,11 +174,11 @@ function createBoard() {
 }
 createBoard();
 
-const allSquare = document.querySelectorAll(".square");
+const allSquares = document.querySelectorAll(".square");
 
-// console.log(allSquare);
+// console.log(allSquares);
 
-allSquare.forEach((square) => {
+allSquares.forEach((square) => {
   square.addEventListener("dragstart", dragStart);
   square.addEventListener("dragover", dragOver);
   square.addEventListener("drop", dragDrop);
@@ -133,8 +204,12 @@ function dragOver(e) {
 
 function dragDrop(e) {
   e.stopPropagation();
-  console.log(e.target);
+  console.log("playerGo", playerGo);
+  console.log("e.target", e.target);
+  const correctGo = draggedElement.firstChild.classList.contains(playerGo);
   const taken = e.target.classList.contains("peice");
+  const opponentGo = playerGo === "white" ? "black" : "white";
+  const takenByOpponent = e.target.firstChild?.classList.contains(opponentGo);
 
   // e.target.parentNode.append(draggedElement)
   // e.target.remove()
@@ -159,7 +234,7 @@ function changePlayer() {
 
 function reverseIds() {
   const allSquares = document.querySelectorAll(".square");
-  allSquare.forEach((square, i) =>
+  allSquares.forEach((square, i) =>
     square.setAttribute("square-id", width * width - 1 - i)
   );
 }
